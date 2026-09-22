dba5f4f2f3ec5fb3c5fd985ecac2bd341854d5c8
noobflick <rishavsanjan0@gmail.com>
2026-09-22 22:29:20 +0530

    added a url-service which shortens the link, no redis or database yet, stores in set

---
+++ url-services/server.js
@@ -0,0 +1,55 @@
+import express from "express"
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json())
+
+const urls = new Map();
+
+app.get("/", (req, res) => {
+    res.json({
+        service: "URL Service",
+        staus: "Running"
+    })
+})
+
+
+app.post("/shorten", (req, res) => {
+    const { url } = req.body;
+
+    if (!url) {
+        return res.status(400).json({
+            error: "URL is required"
+        });
+    }
+
+    const id = Math.random().toString(36).substring(2, 8);
+
+    urls.set(id, url);
+
+    res.status(201).json({
+        shortId: id,
+        url
+    });
+})
+
+
+app.get("/url/:id", (req, res) => {
+    const url = urls.get(req.params.id)
+
+    if (!url) {
+        return res.status(404).json({
+            error: "URL not found"
+        });
+    }
+
+    res.json({
+        url
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`URL service running on port ${PORT}`);
+})
+
