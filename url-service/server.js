675b2860519fc84441ff0f5dbe61cf4ad7eae956
rishavsanjan <rishavsanjan@U-2IR7IM6ONL3VM.corp.amazonworkspaces.com>
2026-09-25 23:16:32 +0530

    added redis caching

--- url-service/server.js
+++ url-service/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import {createClient} from "redis"
 
 const app = express();
 const PORT = 5000;
@@ -6,6 +7,16 @@ const PORT = 5000;
 app.use(express.json())
 
 const urls = new Map();
+
+const redisClient = createClient({
+  url: "redis://redis:6379"
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+await redisClient.connect();
 
 app.get("/", (req, res) => {
     res.json({
@@ -37,13 +48,33 @@ app.post("/shorten", (req, res) => {
 
 app.get("/url/:id", async (req, res) => {
     const shortId = req.params.id;
-    const url = urls.get(req.params.id)
+    const url = urls.get(req.params.id);
+
+    const cachedUrl = await redisClient.get(`url:${shortId}`);
+
+    if (cachedUrl) {
+        console.log("Cache hit for URL:", cachedUrl);
+        return res.json({
+            originalUrl: cachedUrl,
+            source: "redis"
+        });
+    }
+    
+    console.log("CACHE MISS for URL:", url);
 
     if (!url) {
         return res.status(404).json({
             error: "URL not found"
         });
     }
+
+    const result = await pool.query(
+    "SELECT original_url FROM urls WHERE short_id = $1",
+    [id]
+  );
+
+     const originalUrl = result.rows[0].original_url;
+    await redisClient.set(`url:${id}`, originalUrl);
 
     try {
         await fetch("http://analytics-service:6001/track", {
@@ -60,7 +91,8 @@ app.get("/url/:id", async (req, res) => {
     }
 
     res.json({
-        url
+        originalUrl,
+        source: "postgres"
     });
 })
 
